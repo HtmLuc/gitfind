4379936ac81e39eaf944bcad930b8e1f874ad9c3
Lucas Dantas <lucasvdmedeiros@proton.me>
2024-12-31 16:59:40 -0300

    feat: substituição da página estática por dinâmica

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,33 @@
-import './styles.css'
-import { useState } from 'react'
-import { Header } from '../../components/header'
-import ItemList from '../../components/itemList'
-import githubIcon from '../../assets/icone-github.png'
-import profileImage from '../../assets/profile-image.jpg'
+import { useState } from 'react';
+import './styles.css';
+import { Header } from '../../components/header';
+import ItemList from '../../components/itemList';
+import githubIcon from '../../assets/icone-github.png';
 
-const anchorProfile = 'https://github.com/HtmLuc'
+const anchorProfile = 'https://github.com/HtmLuc';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if(newUser.name) {
+      const {avatar_url, bio, login, name} = newUser;
+      setCurrentUser({avatar_url, bio, name, login});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if(newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <Header />
@@ -19,32 +39,45 @@ function App() {
         />
         <div className="info">
           <div>
-            <input name="usuariro" placeholder="@userName" />
-            <button>Buscar</button>
-          </div>
-          <div className="profile">
-            <img
-              src={profileImage}
-              className="userImage"
-              alt="Imagem do usuário"
+            <input
+              name="usuariro"
+              placeholder="@userName"
+              onChange={event => setUser(event.target.value)}
             />
-            <div>
-              <h3>Lucas Dantas</h3>
-              <a href={anchorProfile} target="_blank">@HtmLuc</a>
-              <p>I’m currently undergraduate student of bachelor of information technology at UFRN-IMD.</p>
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {currentUser?.name ?
+            <>
+              <div className="profile">
+                <img
+                  src={currentUser.avatar_url}
+                  className="userImage"
+                  alt="Imagem do usuário"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <a href={anchorProfile} target="_blank">@{currentUser.login}</a>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr/>
+            </>
+          : null}
+          {repos?.length ?
+            <div className="repository">
+              <h4>Repositórios</h4>
+              {repos.map(repo => (
+                <ItemList
+                  title={repo.name}
+                  description={repo.description ? repo.description : "Não há descrição"}
+                />
+              ))}
             </div>
-          </div>
-          <hr/>
-          <div className="repository">
-            <h4>Repositórios</h4>
-            <ItemList title="Título" description="Descrição"/>
-            <ItemList title="Título" description="Descrição"/>
-            <ItemList title="Título" description="Descrição"/>
-          </div>
+          : null}
         </div>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
